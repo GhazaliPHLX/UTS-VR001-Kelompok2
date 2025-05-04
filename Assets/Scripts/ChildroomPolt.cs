63228b2eacb91119ff8da8d7ef2ff346d3e8cdb9
Akbar Ghazali <akb.agh123@gmail.com>
2025-05-04 22:05:01 +0700

    Adding raycast poltergeist

---
+++ Assets/Scripts/ChildroomPolt.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChildroomPolt : MonoBehaviour , ITrigger
+{
+    public AudioSource doorBam;
+    public GameObject door1;
+    public GameObject door2;
+    public void trigger()
+    {
+        //doorBam.Play();
+        CloseDoor doorScript = door1.GetComponent<CloseDoor>();
+        if(doorScript != null)
+        {
+            doorScript.Interact();
+            Debug.Log("DoorClosed");
+        }
+
+        CloseDoor doorScript1= door1.GetComponent<CloseDoor>();
+        if (doorScript1!= null)
+        {
+            doorScript1.Interact();
+            Debug.Log("DoorClosed");
+
+        }
+
+
+    }
+
+    
+
+}
